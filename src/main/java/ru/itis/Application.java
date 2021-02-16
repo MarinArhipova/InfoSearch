1ed5a73c623dd14ee40764f9f0f9d14a0d58a5cf
marina.arkhipova <marina.arkhipova>
2021-02-16 15:09:57 +0300

    edited name of file

--- src/main/java/ru/itis/Application.java
+++ src/main/java/ru/itis/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimbirsoftApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SimbirsoftApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
